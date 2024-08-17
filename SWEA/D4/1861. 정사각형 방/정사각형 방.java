abed7170f4af4934f423812b23c885476d200e03
강민 <124882902+KMsLOG@users.noreply.github.com>
2024-08-17 20:59:11 +0900

    [D4] Title: 정사각형 방, Time: 355 ms, Memory: 29,804 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 
@@ -10,15 +9,19 @@ public class Solution {
 	
 	static int N;
 	static int[][] map;
-	static boolean[][] visited;
-	static int maxcnt;
-	static int cnt;
+	static int maxCnt;
+	static int roomNum;
+	static int maxRoomNum;
 	static int[] dr = {1,-1,0,0};
 	static int[] dc = {0,0,1,-1};
 
-	static void dfs(int r , int c) {
-		visited[r][c] = true;
-		cnt++;
+	static void dfs(int count , int r , int c) {
+		if(maxCnt<count) {
+			maxCnt = count;
+			maxRoomNum = roomNum;
+		} else if(maxCnt == count && maxRoomNum > roomNum) {
+			maxRoomNum = roomNum;
+		}
 		for(int d = 0 ;d<4;d++) {
 			int nr = r + dr[d];
 			int nc = c + dc[d];
@@ -27,8 +30,8 @@ public class Solution {
 				continue;
 			}
 			
-			if(map[nr][nc] == map[r][c]+1 && !visited[nr][nc]) {
-				dfs(nr,nc);
+			if(map[nr][nc] == map[r][c]+1) {
+				dfs(count+1, nr,nc);
 			}
 			
 		}
@@ -43,12 +46,9 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc<=T ; tc++) {
-			maxcnt = 0;
 			N = Integer.parseInt(br.readLine());
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
 			
 			map = new int[N][N];
-			visited = new boolean[N][N];
 			
 			// map에 값넣기
 			for(int i = 0 ; i<N;i++) {
@@ -58,24 +58,18 @@ public class Solution {
 				}
 			}
 			
+			maxRoomNum = 1;
+			maxCnt = 0;
 			
 			for(int i = 0 ; i<N;i++) {
 				for(int j = 0 ; j<N;j++) {
-					visited = new boolean[N][N];
-					cnt = 0;
-					dfs(i,j);
-					if(maxcnt<cnt) {
-						maxcnt = cnt;
-						pq.clear();
-						pq.add(map[i][j]);
-					} else if(maxcnt == cnt) {
-						pq.add(map[i][j]);
-					}
+					roomNum = map[i][j];
+					dfs(1,i,j);
 				}
 			}
 			
 			
-			sb.append("#"+tc+" "+pq.poll()+" "+maxcnt+"\n");
+			sb.append("#"+tc+" "+maxRoomNum+" "+maxCnt+"\n");
 		} // end of tc
 		System.out.print(sb);
 	} // end of main

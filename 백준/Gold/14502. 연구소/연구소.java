bbcc419f906dda6c320831f62d455e3008d1432f
강민 <124882902+KMsLOG@users.noreply.github.com>
2024-08-23 08:29:02 +0900

    [Gold IV] Title: 연구소, Time: 428 ms, Memory: 147684 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -37,12 +37,13 @@ public class Main {
                 }
             }
         }
+        
         List<Point> wall = new ArrayList<>();
-        comb(0, wall,3);
+        comb(0, wall, 3);
         System.out.println(max);
-    } // end of main
+    } 
 
-    static void comb(int start, List<Point> wall,int r) {
+    static void comb(int start, List<Point> wall, int r) {
         if (r == 0) {
             max = Math.max(max, safeArea(wall));
             return;
@@ -50,25 +51,29 @@ public class Main {
 
         for (int i = start; i < avail.size(); i++) {
             wall.add(avail.get(i));
-            comb(i + 1, wall, r-1);
+            comb(i + 1, wall, r - 1);
             wall.remove(wall.size() - 1);
         }
-    } // end of comb
+    }
 
     static int safeArea(List<Point> walls) {
         int[][] copyMap = new int[R][C];
         for (int i = 0; i < R; i++) {
-            copyMap[i] = Arrays.copyOf(map[i],C);
+            System.arraycopy(map[i], 0, copyMap[i], 0, C);
         }
 
         for (Point p : walls) {
             copyMap[p.r][p.c] = 1;
         }
 
+        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[R][C];
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (copyMap[i][j] == 2) {
-                    bfs(copyMap, new Point(i, j));
+                if (copyMap[i][j] == 2 && !visited[i][j]) {
+                    queue.add(new Point(i, j));
+                    visited[i][j] = true;
+                    bfs(copyMap, queue, visited);
                 }
             }
         }
@@ -83,23 +88,20 @@ public class Main {
         }
 
         return cnt;
-    } // end of safeArea
+    }
 
-    static void bfs(int[][] map, Point start) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(start);
-        map[start.r][start.c] = 2;
-
+    static void bfs(int[][] map, Queue<Point> queue, boolean[][] visited) {
         while (!queue.isEmpty()) {
             Point cur = queue.poll();
             for (int d = 0; d < 4; d++) {
                 int nr = cur.r + dr[d];
                 int nc = cur.c + dc[d];
-                if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] == 0) {
+                if (nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] == 0 && !visited[nr][nc]) {
                     map[nr][nc] = 2;
+                    visited[nr][nc] = true;
                     queue.add(new Point(nr, nc));
                 }
             }
         }
-    } // end of bfs
-} // end of class
+    }
+}

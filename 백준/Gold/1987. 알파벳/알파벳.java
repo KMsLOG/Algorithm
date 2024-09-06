7d61564646318b04a7ac04628c78492be47b5563
강민 <124882902+KMsLOG@users.noreply.github.com>
2024-09-06 09:02:46 +0900

    [Gold IV] Title: 알파벳, Time: 700 ms, Memory: 17184 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,8 +8,7 @@ public class Main {
 	static char[][] map;
 	static int[] dr = {1,-1,0,0};
 	static int[] dc = {0,0,1,-1};
-	static List<Character> list = new LinkedList<>();
-	static boolean[][] visited;
+	static boolean[] visited;
 	static int max = 0;
 	
 	public static void main(String[] args) throws IOException {
@@ -22,33 +19,29 @@ public class Main {
 		C = Integer.parseInt(st.nextToken());
 		
 		map = new char[R][C];
-		visited = new boolean[R][C];
+		visited = new boolean[26];
 		for(int i = 0 ; i<R ;i++) {
 			String s = br.readLine();
 			for(int j = 0 ; j<C;j++) {
 				map[i][j] = s.charAt(j);
 			}
 		}
-		
-		list.add(map[0][0]);
-		visited[0][0] =true;
-		dfs(0,0);
+		visited[map[0][0]-'A'] = true;
+		dfs(0,0,1);
 		System.out.println(max);
 	} // end of main
 	
-	static void dfs(int r, int c) {
-		max = Math.max(max, list.size());
+	static void dfs(int r, int c, int cnt) {
+		max = Math.max(max, cnt);
 		
 		for(int d = 0 ; d<4 ;d++) {
 			int nr = r + dr[d];
 			int nc = c + dc[d];
 			try {
-				if(!visited[nr][nc] && !list.contains(map[nr][nc])) {
-					list.add(map[nr][nc]);
-					visited[nr][nc] = true;
-					dfs(nr,nc);
-					list.remove(list.size()-1);
-					visited[nr][nc] = false;
+				if(!visited[map[nr][nc]-'A']) {
+					visited[map[nr][nc] - 'A'] = true;
+					dfs(nr,nc,cnt+1);
+					visited[map[nr][nc] - 'A'] = false;
 				}
 			} catch (Exception e) {}
 		}

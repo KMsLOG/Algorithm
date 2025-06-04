32c0d65c8ba520e0cc63b0960dbc3c2f78d664ed
강민 <124882902+KMsLOG@users.noreply.github.com>
2025-06-04 12:08:07 +0900

    [Silver III] Title: 달팽이, Time: 444 ms, Memory: 48584 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,34 +9,37 @@ public class Main {
         int Num = Integer.parseInt(br.readLine());
         int[][] map = new int[N][N];
 
-        int[] dr = {1,0,-1,0};
-        int[] dc = {0,1,0,-1};
+        int[] dr = {1, 0, -1, 0};
+        int[] dc = {0, 1, 0, -1};
         int[] co = new int[2];
-        int start = N*N;
+        int start = N * N;
         int r = 0;
         int c = 0;
         int d = 0;
-        while(start>0){
+
+        while (start > 0) {
             map[r][c] = start;
-            if(start == Num){
-                co[0] = r+1;
-                co[1] = c+1;
+            if (start == Num) {
+                co[0] = r + 1;
+                co[1] = c + 1;
             }
-            if(r+dr[d]<0 || r+dr[d]>=N || c+dc[d]<0||c+dc[d]>=N||map[r+dr[d]][c+dc[d]]>0){
-                d = (d+1)%4;
+            if (r + dr[d] < 0 || r + dr[d] >= N || c + dc[d] < 0 || c + dc[d] >= N || map[r + dr[d]][c + dc[d]] > 0) {
+                d = (d + 1) % 4;
             }
-
-            r = r+dr[d];
-            c = c+dc[d];
+            r = r + dr[d];
+            c = c + dc[d];
             start--;
+        }
 
-        }
-        for(int i = 0 ; i<N;i++){
-            for(int j = 0 ;j<N;j++) {
-                System.out.print(map[i][j] + " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(map[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
-        System.out.println(co[0]+" "+co[1]);
+        sb.append(co[0]).append(" ").append(co[1]);
+
+        System.out.println(sb);
     } // end of main
 } // end of class

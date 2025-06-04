b06da8ce58982f6414355352dc9d416b89d13f68
강민 <124882902+KMsLOG@users.noreply.github.com>
2025-06-04 12:06:05 +0900

    [Silver III] Title: 달팽이, Time: 4244 ms, Memory: 125700 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int Num = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+
+        int[] dr = {1,0,-1,0};
+        int[] dc = {0,1,0,-1};
+        int[] co = new int[2];
+        int start = N*N;
+        int r = 0;
+        int c = 0;
+        int d = 0;
+        while(start>0){
+            map[r][c] = start;
+            if(start == Num){
+                co[0] = r+1;
+                co[1] = c+1;
+            }
+            if(r+dr[d]<0 || r+dr[d]>=N || c+dc[d]<0||c+dc[d]>=N||map[r+dr[d]][c+dc[d]]>0){
+                d = (d+1)%4;
+            }
+
+            r = r+dr[d];
+            c = c+dc[d];
+            start--;
+
+        }
+        for(int i = 0 ; i<N;i++){
+            for(int j = 0 ;j<N;j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(co[0]+" "+co[1]);
+    } // end of main
+} // end of class

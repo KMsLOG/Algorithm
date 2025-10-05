2c9efae3026ab586337f69e399b50a7ac588e7ab
강민 <124882902+KMsLOG@users.noreply.github.com>
2025-10-05 12:18:24 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1152 ms, Memory: 315240 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -3,69 +3,74 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.StringTokenizer;
-
 
 public class Main {
-    static int N,M,K;
+    static int N, M, K;
     static int[][] map;
     static boolean[][][] visited;
     static int[] dr = {1, -1, 0, 0};
     static int[] dc = {0, 0, 1, -1};
+
+    static class Node {
+        int r, c, broken, dist;
+        Node(int r, int c, int broken, int dist) {
+            this.r = r;
+            this.c = c;
+            this.broken = broken;
+            this.dist = dist;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[2]);
 
         map = new int[N][M];
-        visited = new boolean[K+1][N][M];
+        visited = new boolean[K + 1][N][M];
 
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            char[] row = br.readLine().toCharArray();
             for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j) - '0';
+                map[i][j] = row[j] - '0';
             }
         }
+
         System.out.println(bfs(0, 0));
-    } // end of main
-    static int bfs(int startR, int startC){
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{startR,startC,0});
+    }
+
+    static int bfs(int startR, int startC) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(startR, startC, 0, 1));
         visited[0][startR][startC] = true;
-        int dis = 1;
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i= 0 ;i<size;i++){
-                int[] cur = q.poll();
-                int r = cur[0];
-                int c = cur[1];
-                int broken = cur[2];
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
 
-                if (r == N - 1 && c == M - 1) {
-                    return dis;
+            if (cur.r == N - 1 && cur.c == M - 1) {
+                return cur.dist;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = cur.r + dr[d];
+                int nc = cur.c + dc[d];
+
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+
+                if (map[nr][nc] == 0 && !visited[cur.broken][nr][nc]) {
+                    visited[cur.broken][nr][nc] = true;
+                    q.add(new Node(nr, nc, cur.broken, cur.dist + 1));
                 }
-                for (int d = 0; d < 4; d++) {
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
 
-                    if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
-
-                    if (map[nr][nc] == 0 && !visited[broken][nr][nc]) {
-                        visited[broken][nr][nc] = true;
-                        q.add(new int[]{nr, nc, broken});
-                    } else if (map[nr][nc] == 1 && broken < K && !visited[broken+1][nr][nc]) {
-                        visited[broken+1][nr][nc] = true;
-                        q.add(new int[]{nr, nc, broken+1});
-                    }
+                else if (map[nr][nc] == 1 && cur.broken < K && !visited[cur.broken + 1][nr][nc]) {
+                    visited[cur.broken + 1][nr][nc] = true;
+                    q.add(new Node(nr, nc, cur.broken + 1, cur.dist + 1));
                 }
             }
-            dis++;
         }
 
         return -1;
-    } // end of bfs
+    }
 }
